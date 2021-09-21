a5c56c9d579b898c8f2931af5391fcf179b41f86
VitalikBaiduk <vitalilbaiduj@icloud.com>
2021-09-21 20:11:54 +0300

    new kata

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-function countSheeps(arrayOfSheep) {
-    arrayOfSheep.forEach((item,index) => {
-        let count = 0
-        if (item[index]) {
-            return count++
-        }
-        return count
-    })
-}
-let array1 = [true,  true,  true,  false,
-    true,  true,  true,  true ,
-    true,  false, true,  false,
-    true,  false, false, true ,
-    true,  true,  true,  true ,
-    false, false, true,  true
-];
+function squareSum(numbers) {
+    let squareArr = numbers.map(x => x * x);
+    let result = 0
+    squareArr.forEach(item => {
+        result += item
 
-countSheeps(array1)
+    })
+    return result
+}
